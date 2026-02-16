70c96cdb164b1170ee0cd6b8117e6cd44a9f8414
CarolinaNFeria <rogua20.12@gmail.com>
2026-02-16 20:48:50 +0000

    finish2

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,13 +1,20 @@
-import "../../styles/index.css";
+import React from "react";
 
-function SecondsCounter({ seconds }) {
+
+function SecondsCounter({ seconds, digits = 6 }) {
+  const paddedSeconds = String(seconds).padStart(digits, "0");
+  const digitArray = paddedSeconds.split("");
+
   return (
     <div className="counter-wrapper">
-      <h1 className="title">⏱ Contador de Segundos</h1>
+      <h1 className="title">Contador de Segundos</h1>
       <div className="counter-display">
-        {seconds}
+        {digitArray.map((d, i) => (
+          <div key={i} className="digit-box">
+            {d}
+          </div>
+        ))}
       </div>
-      <p className="subtitle">Tiempo desde que cargó la página</p>
     </div>
   );
 }
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,16 +1,47 @@
 
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom"
 import SecondsCounter from "./components/SecondsCounter.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
-import '../styles/index.css'
+import "../styles/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 0
+    };
+    this.incrementSeconds = this.incrementSeconds.bind(this);
+    this.resetSeconds = this.resetSeconds.bind(this);
+  }
 
-let counter = 0;
+  componentDidMount() {
+    this.timerID = setInterval(this.incrementSeconds, 1000);
+  }
 
-setInterval(() => {
-  counter++;
-  root.render(<SecondsCounter seconds={counter} />);
-}, 1000);
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  incrementSeconds() {
+    this.setState((prevState) => ({
+      seconds: prevState.seconds + 1
+    }));
+  }
+
+  resetSeconds() {
+    this.setState({ seconds: 0 });
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <SecondsCounter seconds={this.state.seconds} digits={6} />
+        <button className="reset-button" onClick={this.resetSeconds}>
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
